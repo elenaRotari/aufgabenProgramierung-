d18a5ca2d117b1ceac460ce6a2553c8ff1fe17e0
Elena Rotari <elena.rotari@icloud.com>
2022-06-29 13:46:18 +0200

    first done

---
+++ 29_06_22Aufgabe1/index.js
@@ -0,0 +1,22 @@
+const meineVorname = "Elena";
+const meinLand = "Moldova";
+let meinLieblingEssen1 = "Cesar Salat";
+let meinLieblingEssen2 = "Cheescake";
+let meinAlt = 37;
+const meinGeburtsJahr = 1985;
+
+console.log(
+  "Hallo ich bin " +
+    meineVorname +
+    " und ich komme aus " +
+    meinLand +
+    ".\nMeine Lieblingsspeisen sind " +
+    meinLieblingEssen1 +
+    " und " +
+    meinLieblingEssen2 +
+    ".\nIch bin " +
+    meinAlt +
+    " Jahre alt und " +
+    meinGeburtsJahr +
+    " geboren."
+);
