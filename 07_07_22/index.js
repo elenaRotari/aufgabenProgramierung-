543ff8a841620b39fc1ebfec7dbcda103313189a
Elena Rotari <elena.rotari@icloud.com>
2022-07-08 10:25:50 +0200

    fine done

--- 07_07_22/index.js
+++ 07_07_22/index.js
@@ -1,11 +1,56 @@
+/**
+ * Author: Fantasia
+ * Projekt: Signup Seite
+ * Feature: Password Validierung
+ * Company: Tranzfer
+ */
+// Password das der Nutzer eingegeben hat
+// Zum Testen verändern
+let password = "test!";
+// State Variablen
+let isPasswordSafe = password ? true : false;
+let errorMessages = [];
 
+// Länge check -> Passwort muss mindestens 6 Buchstaben lang sein
+if (password.length <= 6) {
+  isPasswordSafe = false;
+  errorMessages.push("Password nicht lang genug");
+}
+// console.log(errorMessages);
+// Sonderzeichen check -> Passwort muss mindestens ein Sonderzeichen enthalten
+if (
+  !(
+    password.includes("!") ||
+    password.includes("$") ||
+    password.includes("%") ||
+    password.includes("&") ||
+    password.includes("+") ||
+    password.includes("#")
+  )
+) {
+  isPasswordSafe = false;
+  errorMessages.push("Passwort muss Sonderzeichen enthalten");
+}
+console.log(errorMessages);
 
+// Bekannte Worte check -> Passwort darf keine bekannten Worte enthalten
+if (password.indexOf("password") > 5 || password.indexOf("123456") > 4) {
+  isPasswordSafe = false;
+  errorMessages.push("Passwort darf keine leichten Worte enthalten");
+}
 
-
-
-
-
-
-
-
-
+// Zum Schluss gibt das Programm aus ob das Passwort sicher ist
+console.log("Überprüfung abgeschlossen.");
+console.log(
+  (isPasswordSafe = true
+    ? "Passwort ist sicher."
+    : "Passwort ist nicht sicher!")
+);
+if ((isPasswordSafe = true)) {
+  console.log("Anmeldung kann fortfahren.");
+} else if ((isPasswordSafe = false)) {
+  console.log("Es gab folgende Fehler:");
+} else {
+  console.log(errorMessages.join(""));
+}
+console.log(isPasswordSafe);
--- letter_counter_Exercise/solution.js
+++ letter_counter_Exercise/solution.js
@@ -1,8 +1,6 @@
-const str =
-  "Die split() Methode teilt ein String Objekt in ein Array von Strings auf, indem der String in Teilstrings zerteilt wird, wobei ein angegebenes Trennzeichen verwendet wird, um zu bestimmen, wo die Aufteilung erfolgen soll.";
+const str = "i love you ";
 const modifidString = str.split(" ").join("").toLowerCase();
 console.log(modifidString);
-​
 const result = [];
 for (let i = 0; i < modifidString.length; i++) {
   console.log(modifidString[i]);
@@ -20,16 +18,14 @@ for (let i = 0; i < result.length - 1; i++) {
   }
 }
 console.log(result);
-​
-const countLetters = (str) => {
-  const result = {};
-  for (let i = 0; i < str.length; i++) {
-    Object.keys(result).includes(str[i])
-      ? result[str[i]]++
-      : (result[str[i]] = 1);
-  }
-  return result;
-};
-console.log(countLetters(str));
-
-
+// ​
+// const countLetters = (str) => {
+//   const result = {};
+//   for (let i = 0; i < str.length; i++) {
+//     Object.keys(result).includes(str[i])
+//       ? result[str[i]]++
+//       : (result[str[i]] = 1);
+//   }
+//   return result;
+// };
+// console.log(countLetters(str));
